f8ff67ffbe16f2351a27f234c986b7e7eefe30cb
piesieomane <piesieyeboah@gmail.com>
2022-08-16 11:23:40 +0300

    change provider name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provid } from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import './index.css';
 import App from './App';
