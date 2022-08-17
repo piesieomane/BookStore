dd66ae536bc07d510ad7f55be721e7a28940d5f9
piesieomane <piesieyeboah@gmail.com>
2022-08-17 22:12:31 +0300

    remove console.log from booksholder file

--- src/components/BooksHolder.jsx
+++ src/components/BooksHolder.jsx
@@ -1,16 +1,10 @@
-// import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-// import axios from 'axios';
+
 import Books from './Books';
 import AddBook from './AddBook';
-// import { fetchBOOK } from '../redux/books/books';
-// import API from './api';
 
 const BooksHolder = () => {
   const books = useSelector((state) => state.books);
-  // const dispatch = useDispatch();
-  console.log('BOOKS6', books);
-
 
   return (
     <div>
