f5cd74abc566b19b947c4325cfe8f0154ed9b664
piesieomane <piesieyeboah@gmail.com>
2022-08-18 19:57:17 +0300

    Add styles to navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { BsPersonCircle } from 'react-icons/bs';
 import style from './Nav.module.css';
 
 const LINKS = [
@@ -10,13 +11,16 @@ const LINKS = [
 const Nav = () => (
   <div className={style.navflex}>
     <h1>Bookstore CMS</h1>
-    <ul>
+    <ul className={style.navlinks}>
       {LINKS.map((link) => (
         <li key={link.to}>
-          <NavLink to={link.to}>{link.text}</NavLink>
+          <NavLink to={link.to} className={style.anchor}>{link.text}</NavLink>
         </li>
       ))}
     </ul>
+    <div className={style.user}>
+      <BsPersonCircle />
+    </div>
   </div>
 );
 
